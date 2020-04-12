7ebb07bb106059be0980d3dc5906ff9b54c905b3
Stephen Dodd <skyledodd1989@gmail.com>
2020-04-12 18:39:36 -0500

    complete

--- main.js
+++ main.js
@@ -1,19 +1,46 @@
-function formatQueryParams() {
-
+function formatRequest(user) {
+    let url = `https://api.github.com/users/${user}/repos`;
+    console.log('formatRequest ran')
+    getUserRepos(url)
 }
 
 function listenForSubmit() {
+    let userName = $('#user-name').val('');
     $('form').submit(e => {
+        $('ul').empty();
         e.preventDefault();
         let userName = $('#user-name').val();
         console.log(userName);
+        
+    formatRequest(userName);
     });
 
-    formatQueryParams();
 }
 
-function getUserRepos() {
+function getUserRepos(link) {
+    console.log(`getUserRepos ran with ${link}`)
+    fetch(link)
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(err => {
+        $('.hidden').text('something went wrong');
+    });
+    
+}
 
+function displayResults(responseJson) {
+    $('#user-name').val('');
+    const results = responseJson;
+    console.log(results);
+    for (let i = 0; i < results.length; i++) {
+        $('ul').append(`<li>
+        <a href='https://github.com/${results[i]["full_name"]}'>${results[i]['name']}</a></li>`);
+    }
 }
 
 function handleEvents() {
